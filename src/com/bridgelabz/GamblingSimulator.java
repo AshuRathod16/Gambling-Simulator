f9e4930e801e8b50972c7187fa243603431073bf
Ashwini Rathod <rashwini510@gmail.com>
2022-06-04 22:36:20 +0530

    Added:Gambler make bet either win or loose

--- src/com/bridgelabz/GamblingSimulator.java
+++ src/com/bridgelabz/GamblingSimulator.java
@@ -3,6 +3,18 @@ package com.bridgelabz;
 public class GamblingSimulator {
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling Simulator");
+        int bet = 1;
+        int totalAmount = 100;
+        double gameCheck = Math.floor(Math.random() * 10) % 2;
+        if (bet == gameCheck) {
+            System.out.println("Gambler win");
+            totalAmount++;
+        } else {
+            System.out.println("Gambler loose");
+            totalAmount--;
+
+        }
+        System.out.println("Total amount :" + totalAmount);
     }
 
 }
