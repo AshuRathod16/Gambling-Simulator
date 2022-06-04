eee67226f5536dead6c9a93e1914d3e65b0fa86a
Ashwini Rathod <rashwini510@gmail.com>
2022-06-04 22:42:05 +0530

    Added:Starting od game

--- src/com/bridgelabz/GamblingSimulator.java
+++ src/com/bridgelabz/GamblingSimulator.java
@@ -3,18 +3,9 @@ package com.bridgelabz;
 public class GamblingSimulator {
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling Simulator");
-        int bet = 1;
-        int totalAmount = 100;
-        double gameCheck = Math.floor(Math.random() * 10) % 2;
-        if (bet == gameCheck) {
-            System.out.println("Gambler win");
-            totalAmount++;
-        } else {
-            System.out.println("Gambler loose");
-            totalAmount--;
+        int betPerGame = 1;
+        int startingTotalAmount = 100;
 
-        }
-        System.out.println("Total amount :" + totalAmount);
     }
 
 }
