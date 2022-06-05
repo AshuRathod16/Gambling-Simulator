9a6a13d2825dec5f4ffb96f6d37382e3e76811d9
Ashwini Rathod <rashwini510@gmail.com>
2022-06-05 13:11:13 +0530

    Added:Calculate the 50% stake if gambler won or loose

--- src/com/bridgelabz/GamblingSimulator.java
+++ src/com/bridgelabz/GamblingSimulator.java
@@ -3,8 +3,20 @@ package com.bridgelabz;
 public class GamblingSimulator {
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling Simulator");
-        int betPerGame = 1;
-        int startingTotalAmount = 100;
+        int totalAmount = 100;
+        int bet = 1;
+        while (totalAmount > 50 && totalAmount < 150) {
+            double gameCheck = Math.floor(Math.random() * 10) % 2;
+            if (bet == gameCheck) {
+                System.out.println("you won the game");
+                totalAmount++;
+            } else {
+                System.out.println("you lost the game");
+                totalAmount--;
+            }
+        }
+        System.out.println("Total amount :" + totalAmount);
+
 
     }
 
